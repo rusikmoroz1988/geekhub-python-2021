901aedac7764296d341bfbe9b2e4ce661da36004
Ruslan Moroz <rusikmoroz1988@gmail.com>
2021-11-16 23:08:32 +0300

    Adding comments

--- НТ_1/1.py
+++ НТ_1/1.py
@@ -1,3 +1,7 @@
+####################################Tasks:
+# Write a script which accepts a sequence of comma-separated numbers 
+# from user and generate a list and a tuple with those numbers.
+
 sequ_of_numbers = input("Input sequence of numbers:")
 list_of_numbers = sequ_of_numbers.split(',')
 print("List : ", list_of_numbers)
