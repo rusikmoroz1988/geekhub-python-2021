7f447e0a784caf9e281e202a76c440768a006788
Ruslan Moroz <rusikmoroz1988@gmail.com>
2021-11-16 22:57:59 +0300

    The first task is solved

---
+++ НТ_1/1.py
@@ -0,0 +1,6 @@
+sequ_of_numbers = input("Input sequence of numbers:")
+list_of_numbers = sequ_of_numbers.split(',')
+print("List : ", list_of_numbers)
+
+tuple_of_numbers = tuple(list_of_numbers)
+print("Tuple : ", tuple_of_numbers)
