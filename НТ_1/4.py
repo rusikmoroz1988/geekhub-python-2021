cb851ca8352112f3ef3a7834163c33e3e3815a4e
Ruslan Moroz <rusikmoroz1988@gmail.com>
2021-11-17 21:08:02 +0300

    The fourth task is solved (removed the space)

--- НТ_1/4.py
+++ НТ_1/4.py
@@ -5,4 +5,4 @@ number_of_lines = int(input("Input the number of lines:"))
 sentence = []
 for line in range(0, number_of_lines):
     sentence.append(input("Input text:"))
-print(' '.join(sentence))
+print(''.join(sentence))
