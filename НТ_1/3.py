01c8d5d07f6be19f120147ed4bd1bc46723b1962
Ruslan Moroz <rusikmoroz1988@gmail.com>
2021-11-18 17:43:54 +0300

    The third task is solved (bug fix)

--- НТ_1/3.py
+++ НТ_1/3.py
@@ -2,5 +2,5 @@
 # Write a script to sum of the first n positive integers.
 
 first_numbers = int(input("Enter the number of positive numbers:"))
-list_numbers = [num for num in range(0,first_numbers)]
+list_numbers = [num for num in range(1, first_numbers + 1)]
 print(sum(list_numbers))
