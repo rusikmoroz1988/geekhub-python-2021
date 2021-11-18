056618bdc36926d3175428514229fd1ca740ebad
Ruslan Moroz <rusikmoroz1988@gmail.com>
2021-11-18 09:12:15 +0300

    The third task is solved (generator use)

--- НТ_1/3.py
+++ НТ_1/3.py
@@ -2,7 +2,5 @@
 # Write a script to sum of the first n positive integers.
 
 first_numbers = int(input("Enter the number of positive numbers:"))
-summ = 0
-for num in range(0, first_numbers):
-    summ+=num
-print(summ)
+list_numbers = [num for num in range(0,first_numbers)]
+print(sum(list_numbers))
