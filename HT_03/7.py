c80ac83a89bc8f1ac9b28244ac7cb25630ac7349
Ruslan Moroz <rusikmoroz1988@gmail.com>
2021-11-24 00:43:02 +0300

    The seventh task is solved

---
+++ HT_03/7.py
@@ -0,0 +1,22 @@
+####################################Task:
+# Ну і традиційно -> калькулятор :) повинна бути 1 ф-цiя яка б приймала 3 аргументи 
+# - один з яких операцiя, яку зробити!
+
+def calculate(number_1, number_2, operation):
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+    else:
+        print('Data entered incorrectly!')
+
+data_list = input('Enter math operation you would like to complete (for example, 8 + 7): ').split(" ")
+calculate(eval(data_list[0]), eval(data_list[2]), data_list[1])
