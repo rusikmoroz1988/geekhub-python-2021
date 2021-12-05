073c64f002e5b9835cde59b47b3922e08eafcd9d
Ruslan Moroz <rusikmoroz1988@gmail.com>
2021-12-05 20:11:17 +0300

    The first task is solved (bug fixed)

--- HT_06/1.py
+++ HT_06/1.py
@@ -20,21 +20,24 @@
 
 import time
 
-def print_colors(colors_avto, colors_ped, step = 0):
-    try:
-        item_avto = colors_avto[step]
-        item_ped = colors_ped[step]
-        print(item_avto, (15 - len(item_avto) - len(item_ped)) * " ", item_ped)
-        step+=1
-    except:
-        step=0          
+def print_colors(colors_avto, colors_ped, step):
+    item_avto = colors_avto[step]
+    item_ped = colors_ped[step]
+    print(item_avto, (15 - len(item_avto) - len(item_ped)) * " ", item_ped)  
     time.sleep(1)
-    print_colors(colors_avto, colors_ped, step)
     
-
 def start_traffic_light():
     colors_avto = 4*['Red'] + 2 *['Yellow'] + 4*['Green'] + 2*['Yellow']
     colors_ped = 6*['Green'] + 6 *['Red']
-    print_colors(colors_avto, colors_ped)
-
+    
+    step = 0
+    while True: 
+        try:
+            print_colors(colors_avto, colors_ped, step)
+            step+=1
+        except KeyboardInterrupt:
+            break
+        except IndexError:
+           step = 0
+        
 start_traffic_light()
